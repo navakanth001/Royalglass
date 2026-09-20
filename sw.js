fab5513f7cf43a5149523e2e51f9bf384be72dc0
navakanth001 <navakanth001@gmail.com>
2026-09-20 22:17:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inventory-app-v19';
+const CACHE_NAME = 'inventory-app-v20';
 const APP_SHELL = [
   './',
   './index.html',
@@ -30,7 +30,7 @@ self.addEventListener('fetch', event => {
       caches.match(event.request).then(cached =>
         cached || fetch(event.request).then(response => {
           const copy=response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
           return response;
         })
       )
